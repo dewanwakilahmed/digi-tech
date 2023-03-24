e142c63f415614ce0fbe87089d7d7355e6a9d86d
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-03-24 20:47:43 +0600

    Project Initial Setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
+// App State Management
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+
+// MAIN APP
 import App from './App';
+
+// App Performance
 import reportWebVitals from './reportWebVitals';
+
+// CSS
 import './index.css';
 
 const container = document.getElementById('root');
@@ -17,7 +25,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
